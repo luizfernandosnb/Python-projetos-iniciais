14362e2df5fa0598cdf8e38241847899dc6cceed
Luiz Schimidt <luizfernandosnb@gmail.com>
2026-08-28 12:01:08 -0300

    soma

--- Python testes/Python estudos/exe5.py
+++ Python testes/Python estudos/exe5.py
@@ -2,8 +2,11 @@ print ('==' * 15)
 print ('------Comercio Universal------')
 print ('==' * 15)
 avanco = 'S'
+soma = 0 
 while avanco == 'S':
     item = str(input('Digite o nome do produto: ')).strip()
-    valor = int(input('Digite o valor do produto: '))
+    valor = float(input('Digite o valor do produto: '))
     avanco = str(input('Deseja continuar? [S/N]')).upper().strip()
     print ('==' * 15)
+    soma = valor + soma
+print (f'O valor total gasto é: R$ {soma:.2f}')
