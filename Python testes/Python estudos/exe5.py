ae662bbfa4a607720bbb5e01381abc87e825e98f
Luiz Schimidt <luizfernandosnb@gmail.com>
2026-08-29 11:55:48 -0300

    Update exe5.py

--- Python testes/Python estudos/exe5.py
+++ Python testes/Python estudos/exe5.py
@@ -5,6 +5,7 @@ avanco = 'S'
 soma = 0 
 menor_valor = 0
 nome = ' '
+total = 0
 while avanco == 'S':
     i = str(input('Digite o nome do produto: ')).strip()
     v = float(input('Digite o valor do produto: ').replace(',', '.'))
@@ -17,5 +18,8 @@ while avanco == 'S':
         menor_valor = v
         nome = i
     soma = v + soma
-print (f'O valor total gasto é: R$ {soma:.2f}')
+    if v >= 1000:
+        total += 1
+print (f'No total você comprou {total} de produtos com valor superior ou igual a R$ 1000,00.')
 print (f'O produto de menor valor seria {nome} de custo: R$ {menor_valor}')
+print (f'O valor total gasto é: R$ {soma:.2f}')
