403c1a97ced55a43d8096dd37b7f55b7b9fc8c50
Luiz Schimidt <luizfernandosnb@gmail.com>
2026-08-29 11:40:33 -0300

    Update exe5.py

--- Python testes/Python estudos/exe5.py
+++ Python testes/Python estudos/exe5.py
@@ -6,10 +6,16 @@ soma = 0
 menor_valor = 0
 nome = ' '
 while avanco == 'S':
-    item = str(input('Digite o nome do produto: ')).strip()
-    valor = float(input('Digite o valor do produto: ').replace(',', '.'))
+    i = str(input('Digite o nome do produto: ')).strip()
+    v = float(input('Digite o valor do produto: ').replace(',', '.'))
     avanco = str(input('Deseja continuar? [S/N]')).upper().strip()
     print ('==' * 15)
-    soma = valor + soma
+    if menor_valor == 0:
+        menor_valor = v
+        nome = i
+    if menor_valor > v:
+        menor_valor = v
+        nome = i
+    soma = v + soma
 print (f'O valor total gasto é: R$ {soma:.2f}')
 print (f'O produto de menor valor seria {nome} de custo: R$ {menor_valor}')
